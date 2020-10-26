e1ee230be5377641f4b72a0dc3021ca2f0d2c25c
Vasyl Biliba <58170018+spiritufuerte@users.noreply.github.com>
2020-10-27 01:02:38 +0200

    finished first section

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import './App.css';
+import './App.module.scss';
 import Header from "./components/Header/Header";
 import Content from "./components/Content/Content";
 import Footer from "./components/Footer/Footer";
+import classes from './App.module.scss';
+import linesCopy from "./assets/image/lines-copy-3.svg";
+import boxesImage from './assets/image/Boxes.png';
 
 export function App() {
-  return (
-    <div className="App">
-        <Header />
-        <Content />
-        <Footer />
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className={classes.linesWrapper}>
+                <img src={linesCopy} alt="Lines Copy" title="Lines Copy"/>
+            </div>
+            <Header/>
+            <Content/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import classes from './Content.module.scss';
 
 
-const Header = () => {
+const Content = () => {
     return (
-        <div>das</div>
+        <main>
+            <section className={classes.mainSection}>
+                <div className={classes.boxesImageWrapper}>
+                </div>
+                <div className={classes.fruitsImageWrapper}>
+                </div>
+                <div className={classes.title}>FOR A SMARTER SUPPLY CHAIN</div>
+                <div className={classes.titleInfo}>
+                    Take your business to the next level with Tinvio’s Supplier Platform
+                </div>
+                <button>Get Started Now</button>
+
+            </section>
+        </main>
     )
 }
 
-export default  Header;
+export default  Content;
