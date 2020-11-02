d6d07debe1c552f5d95fcbfe04524a2098c3c448
Vasyl Biliba <58170018+spiritufuerte@users.noreply.github.com>
2020-11-02 18:38:21 +0200

    added photo

--- src/components/Content/CommunicationSection/CommunicationSection.jsx
+++ src/components/Content/CommunicationSection/CommunicationSection.jsx
@@ -5,14 +5,13 @@ import beforeImage from '../../../assets/image/Before _ After/Before.png';
 const CommunicationSection = () => {
     return (
         <section className={classes.communicationWrapper}>
-            <div className={classes.beforeImageWrapper}><img src={beforeImage} title='before image'/></div>
+            <div className={classes.beforeImageWrapper}><img src={beforeImage} title='before image' alt='image'/></div>
             <div className={classes.infoWrapper}>
                 <div className={classes.header}>Communication</div>
                 <div className={classes.buttonsWrapper}>
                     <button>BEFORE</button>
                     <button id={classes.after}>AFTER</button>
                 </div>
-                <div className={classes.rectangle}></div>
                 <div className={classes.textHeader}>Old-School Chaos</div>
                 <div className={classes.textBody}>Unstructured orders across multiple apps and inboxes. New message
                     notifications and alerts every
